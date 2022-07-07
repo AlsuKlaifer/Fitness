0bcfe6f33b0e5034e751febca3e1a9c22b576765
Алсу Фаизова <silicium.xxx@gmail.com>
2022-07-07 09:38:35 +0300

    1

---
+++ app/src/main/java/com/example/fitness/trainings/Training.kt
@@ -0,0 +1,10 @@
+package com.example.fitness.trainings
+
+import androidx.drawerlayout.R
+
+data class Training(
+    val id: Int,
+    val name: String,
+    val picture: String,
+    val text: String,
+)
